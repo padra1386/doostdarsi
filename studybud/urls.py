f1df4888ee896166a96e14f040a8198c4c15a215
padra1386 <padrahani@gmail.com>
2022-04-19 20:50:06 +0430

    heroku deploy 3

--- studybud/urls.py
+++ studybud/urls.py
@@ -9,9 +9,7 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    path('api/', include('base.api.urls'))
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('api/', include('base.api.urls')), 
 ]
 
 
